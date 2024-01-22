a76c77d3d76c8a86a8203ca81ed9c6c4c5d791ae
cliffgor <clierozigy@gmail.com>
2024-01-22 10:49:07 +0300

    Add MongoDB connection and dotenv dependency

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log('Database connected');
+    })
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
