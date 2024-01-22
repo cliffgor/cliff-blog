728cf54a8189bceeaa826d826669d98269ed199d
cliffgor <clierozigy@gmail.com>
2024-01-22 23:09:36 +0300

    Add authentication functionality

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    // Hashed password
+
+    const hashPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashPassword,
+    });
+
+    try {
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -11,9 +12,11 @@ mongoose.connect(process.env.MONGO_URI).then(
     })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
